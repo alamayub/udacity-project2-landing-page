cb40712815a197978cf0cb97a0cff879f536843e
Ayub Alam <alamayub85@gmail.com>
2020-04-05 18:47:48 +0530

    some changes

--- js/index.js
+++ js/index.js
@@ -1,16 +1,9 @@
-const links = ['Home','Services', 'About', 'Team', 'Gallery', 'Tuition', 'Contact'];
+const links = ['Services', 'About', 'Team', 'Gallery', 'Tuition', 'Contact'];
 for(let link of links){
     var li = document.createElement('li');
     li.innerHTML=`<a href='#${link}' class='item'>${link}</a>`;
     document.getElementById('ul').appendChild(li);
 }
-/*document.addEventListener('scroll', () => {
-    if(window.scrollY >= 920) {
-        document.querySelector('nav').style.background = 'rgba(18,72,120, 1)';
-    } else {
-        document.querySelector('nav').style.background = 'rgba(18,72,120, .5)';
-    }
-}, false);*/
 
 document.addEventListener('click', function(event) {
     if(event.target.classList.contains('item')) {
